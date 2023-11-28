6add6d8c14053914d2a66475412d74912c52eb47
Azi-C <aaziza.chbihi@gmail.com>
2023-11-28 15:37:36 -0800

    modifications 1

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for i in range(1,101):
-        match i:
-            case (i % 3) == 0:
-                print("{}".format(i), end=' ')
-            case (i % 3) == 0 and (i % 5) == 0:
-                print("{}".format(i), end=' ')
-            case _:
-                print("{}".format(i), end =' ')
+        if (i % 3) == 0:
+            print("{}".format(i), end=' ')
+        elif (i % 3) == 0 and (i % 5) == 0:
+            print("{}".format(i), end=' ')
+        else:
+            print("{}".format(i), end =' ')
