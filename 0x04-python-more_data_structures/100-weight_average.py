b9f0ddbd568b41885076877d493ac4dcda4b4ae1
Azi-C <aaziza.chbihi@gmail.com>
2023-12-08 15:41:40 -0800

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is not None:
+        w = sum(b for a, b in my_list)
+        return (sum(a*b for a, b in my_list)/f) if w > 0 else 0
+    return (0)
