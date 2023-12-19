ad8e9bcc0b4e32b83a2ae92f72d555df234956b7
Azi-C <aaziza.chbihi@gmail.com>
2023-12-18 17:29:17 -0800

    task 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return (fct(*args))
+    except Exception as err:
+        stderr.write("Exception: {}\n".format(err))
+        return None
