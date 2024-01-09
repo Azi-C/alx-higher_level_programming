37b7db71c3c74c8d70ce1b1375feb76419c0d3d0
Azi-C <aaziza.chbihi@gmail.com>
2024-01-09 13:57:58 -0800

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+""" class Square"""
+
+
+Rectangle = __import('9-rectangle')Rectangle
+
+
+class Square(Rectangle):
+    """inherits from Rectangle"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        super().integer_validator("size", size)
+        super.__init__(size, size)
+        self.__size = size
