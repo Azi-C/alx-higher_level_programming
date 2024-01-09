03ff683aba733bd09ec4b32442c21cb71c72e784
Azi-C <aaziza.chbihi@gmail.com>
2024-01-09 13:18:18 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class"""
-    return (type(obj) is not a_class and issubclass(type(obj, a_class)))
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
