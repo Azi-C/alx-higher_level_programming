999d7093c6b5b07f9da84721c90e5fe9db82e422
Azi-C <aaziza.chbihi@gmail.com>
2024-01-21 14:16:39 -0800

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     """Rectangle class inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle"""
-        super()__.init__(id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
