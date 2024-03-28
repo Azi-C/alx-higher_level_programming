9777b02d147604fcb997d328fed7b6582c468de7
Azi-C <aaziza.chbihi@gmail.com>
2024-03-27 19:20:02 -0700

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""find peak function"""
+
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers"""
+
+    size = len(list_of_integers)
+    if size == 0:
+        return None
+
+    left = 0
+    right = size - 1
+    li = list_of_integers
+
+    while left < right:
+        mid = (left + right) // 2
+        if li[mid] > li[mid - 1] and li[mid] > li[mid + 1]:
+            return li[mid]
+        elif li[mid] < li[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return li[left]
