e4a64f868b1e07d80a81549b4a92c6a863c61edf
Azi-C <aaziza.chbihi@gmail.com>
2024-04-01 18:35:46 -0700

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays
+he body of the response (decoded in utf-8)
+"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(argv[1]) as rsp:
+            print(rsp.read().decode('utf-8'))
+    except urllib.error.HTTPERROR as err:
+        print("Error code: {}".formt(err.code))
