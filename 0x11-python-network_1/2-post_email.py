2c1b9094b5e2474b47a13658ceee4bf180c04c82
Azi-C <aaziza.chbihi@gmail.com>
2024-04-01 18:26:34 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
- script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response 
+Script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
 """
 import urllib.request
-from sys import argv
 import urllib.parse
+from sys import argv
 
 
 if __name__ == "__main__":
@@ -12,5 +14,5 @@ if __name__ == "__main__":
     value = {'email': argv[2]}
     data = urllib.parse.urlencode(value).encode('ascii')
     req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as rsp:
-        print(rsp.read().decode('utf-8'))
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
